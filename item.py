3f3bc739ba6c0e7e5de3d46151369438eee397b9
David Ikin <shadow2272@aol.com>
2021-12-14 18:57:31 +0000

    first commit

---
+++ item.py
@@ -0,0 +1,21 @@
+# zone1 - most used stock, zone2 - long storage.
+class Item:
+    def __init__(self, name, category, qty, isUsed=True):
+        self.name = name
+        self.category = category
+        self.qty = qty
+        self.isUsed = isUsed
+        self.zone = []
+
+    def storeItem(self, location):
+        self.zone.append(location)
+    
+    def locateItem(self):
+        methodName = str(self.name)
+        print(methodName, self.zone)
+
+    def getSKU(self):
+        firstHalf = self.name[:2]
+        secondHalf = self.category[:2]
+        print(firstHalf + secondHalf)
+
