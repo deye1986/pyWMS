0f3f1342eb8dc8becb55b60953edfbd2a75ef782
David Ikin <shadow2272@aol.com>
2021-12-20 23:37:01 +0000

    cleaned code

--- item.py
+++ item.py
@@ -1,6 +1,9 @@
 # zone1 - most used stock, zone2 - long storage.
 # S.K.U. = Stock Keeping Unit; the items unique identifier in warehouse operations.
 
+from random import randint
+
+
 class Item:
     '''An item to be stored, location determined by isUsed, which value defaults to True'''
     def __init__(self, name, category, qty, isUsed=True):
@@ -10,21 +13,22 @@ class Item:
         self.isUsed = isUsed
         self.zone = []
 
-    def getSKU(self):
+    def generateSKU(self):
         '''Usually initially called from the storeItem method.'''
-
         # combination of first three characters of the name and category to make an Stock Keeping Unit(sku).
+        # now generates a random 3 digit integer to add to the SKU to avoid duplicate codes.
 
         firstHalf = self.name[:3]
         secondHalf = self.category[:3]
-        return firstHalf + secondHalf    
+        id = str(randint(100, 999))
+        return id + firstHalf + secondHalf    
     
     
     def storeItem(self):
         '''Creates and stores details of the item in the inventory.txt file'''
 
         stockRecord = []
-        self.createSKU = self.getSKU()
+        self.createSKU = self.generateSKU()
         self.itemDetails = [self.name, self.category, self.qty, self.isUsed, self.createSKU]
         print(' ** Alert ** Creating inventory : ' + str(self.itemDetails))
         itemLocation = self.placeItem()
@@ -34,7 +38,6 @@ class Item:
             inventoryFile.write(str(self.itemDetails + self.zone) + '\n')
             inventoryFile.close()
         
-
 
     def placeItem(self):
         '''Called within storeItem() to determine stock placement based on isUsed parameter'''
@@ -46,9 +49,6 @@ class Item:
             z2 = self.zone.append('Zone 2')
             return z2
     
-    def locateItem(self): # test method.
+    def locateItem(self): # test method for development.
         methodName = str(self.name)
         print(methodName, self.zone)
-
-
-
