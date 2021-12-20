2003d174cde1c8a629d4a13aa985420799abaef1
David Ikin <shadow2272@aol.com>
2021-12-20 23:36:17 +0000

    binded functions to the GUI

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import tkinter as tk
-from item import Item
+from tests import runTests
 
 
 window = tk.Tk() # Top level (root window)
+demoLoaded = False
 
-window.title('test')
+def demoButtonClicked():
+    return runTests()
 
-window.minsize(width=800, height=600)
+def refreshStatusButtonClicked():
+    try:
+        with open('inventory.txt', 'r') as invDataCheck:
+            if invDataCheck != True:
+                programStatusFine.grid(row=1, column=3)
+                invDataCheck.close()
+    except:
+        programStatusError.grid(row=1, column=3)
+
+
+window.title('Stocklink Lite - v.0 development build - by David Ikin')
+window.minsize(width=800, height=400)
 
 label = tk.Label(
-    text='Stocklink Lite',
+    text='Stocklink Lite: Prototype build v.0',
     font='Ariel',
     bg='white',
     width=40,
@@ -21,51 +34,54 @@ entryText = tk.Label(
     height = 2,
     width=24
 )
-
 programStatusLabelChecking = tk.Label(
     text='Status check...',
     bg='blue',
-    width=24
+    width=24,
+    height=2
 )
 programStatusError = tk.Label(
-    text='Error: No data',
+    text='Error: No data.',
     bg='red',
-    width=24
+    width=24,
+    height=2
 )
 programStatusFine = tk.Label(
     text='System Online.',
     bg='green',
-    width=24
+    width=24,
+    height=2
 )
-
 entry = tk.Entry(
     width=75,    
 )
-
 searchButton = tk.Button(
     width=24,
     text='search'
 )
-
-
+generateInventoryButton = tk.Button(
+    width=18,
+    height=3,
+    text='Create Demo Inventory',
+    command=demoButtonClicked
+)
+refreshStatusButton = tk.Button(
+    width=18,
+    height=3,
+    text='Refresh',
+    command=refreshStatusButtonClicked
+)
 
 label.grid(row=1, column=0, columnspan=2)
 
 programStatusLabelChecking.grid(row=1, column=3)
-try:
-    with open('inventory.txt', 'r') as invDataCheck:
-        if invDataCheck != True:
-            programStatusFine.grid(row=1, column=3)
-            invDataCheck.close()
-
-except:
-    programStatusError.grid(row=1, column=3)
 
 entry.grid(row=2, column=1, columnspan=1)
 searchButton.grid(row=2, column=3, columnspan=1)
 entryText.grid(row=2, columnspan=1, column=0)
 
-statusCheck = ''
+generateInventoryButton.grid(row=3, column=0)
+refreshStatusButton.grid(row=4, column=0)
 
 
 window.mainloop()
